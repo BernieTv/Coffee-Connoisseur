a71aa295e7e486b8dfd01601b7d1378a5ee7fbdb
Bekzod Tukhtasinov <86732898+BernieTv@users.noreply.github.com>
2022-07-10 14:34:09 +0500

    starting working with geolocation API

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -62,7 +62,7 @@ const CoffeeStore = (props) => {
 				<div className={styles.col1}>
 					<div className={styles.backToHomeLink}>
 						<Link href='/'>
-							<a>Back to home</a>
+							<a>&#129060; Back to home</a>
 						</Link>
 					</div>
 					<div className={styles.nameWrapper}>
