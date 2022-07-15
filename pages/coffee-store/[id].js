3776936d33d9c96846d2597d85d11d88a8655213
Bekzod Tukhtasinov <86732898+BernieTv@users.noreply.github.com>
2022-07-15 14:56:33 +0500

    changes for lighthouse #2

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -165,8 +165,8 @@ const CoffeeStore = (initialProps) => {
 							imgUrl ||
 							'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'
 						}
-						width={600}
-						height={360}
+						width={588}
+						height={330}
 						alt={name}
 						className={styles.storeImg}
 					/>
